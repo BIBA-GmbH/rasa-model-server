6204f5da9dac6703bf0fce6afcf4053af39f5f0c
wel <wel@biba.uni-bremen.de>
2022-08-15 12:54:13 +0200

    extended config.py with variables for upload + comments

--- config.py
+++ config.py
@@ -1,4 +1,21 @@
 from os import environ
 
-models_dir = environ.get('MODELS_DIR', 'models')
+# --- Connection-related variables.
 server_port = environ.get('PORT', 8080)
+
+# --- Download-related variables.
+download_folder = environ.get('DOWNLOAD_FOLDER', 'models')
+
+# --- Upload-related variables.
+# Default upload folder is the model's folder.
+upload_folder = environ.get('UPLOAD_FOLDER', download_folder)
+
+allowed_extension = environ.get(
+    'ALLOWED_EXTENSIONS',
+    {
+        'gz',
+    }
+)
+
+# Maximum allowed file size in MB
+maximum_file_size = 150 * 1000 * 1000
